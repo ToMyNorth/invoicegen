c1c76dd9cefbfb040d97b991ec5bc7b7af0925c1
ToMyNorth <380816700@qq.com>
2026-07-22 22:47:00 +0800

    Remove invalid aggregate rating markup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -85,11 +85,6 @@ export default function RootLayout({
           price: '0',
           priceCurrency: 'USD',
         },
-        aggregateRating: {
-          '@type': 'AggregateRating',
-          ratingValue: '4.8',
-          ratingCount: '156',
-        },
         featureList: [
           'Free invoice generation',
           'PDF download',
